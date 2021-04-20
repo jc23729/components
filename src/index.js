796eb6ee935155dbda766bd897fbe1b4055b1cea
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-20 16:26:12 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-    return <div>Hi there</div>;
+  return (
+    <div classsName="ui container comments">
+      <div classsName="ui container comments">
+        <a href="/" className="Avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+                  <div className="metadata"></div>
+                  <span className="date">Today at 6:00pm</span>
+              </div>
+              <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
