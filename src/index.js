4a6223f7992f61514b4b05a92004fcd39581f036
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-20 19:00:05 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
-    <CommentDetail />
+    <div classsName="ui container comments">
+      <CommentDetail />
+    </div>
   );
 };
 
