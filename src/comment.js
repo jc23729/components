d0a4a10a562401f0c536074cb4e5c5f35bf77a70
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-20 18:48:55 -0400

    rearrange component

--- src/comment.js
+++ src/comment.js
@@ -1,7 +1,50 @@
-
+import faker from "faker";
 
 const comments = () => {
-    return (
-        
-    )
-}
+  return (
+    <div classsName="ui container comments">
+      <div classsName="comment">
+        <a href="/" className="Avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata"></div>
+          <span className="date">Today at 6:00pm</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+
+      <div classsName="comment">
+        <a href="/" className="Avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata"></div>
+          <span className="date">Today at 6:00pm</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+      <div classsName="comment">
+        <a href="/" className="Avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata"></div>
+          <span className="date">Today at 6:00pm</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+  );
+};
+
+export default comments;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import "./comment"
 
 const App = () => {
   return (
