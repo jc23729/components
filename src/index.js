e3dc325d2dce448677692b1c2d820d8c837b0999
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-20 18:10:20 -0400

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,34 @@ import faker from "faker";
 const App = () => {
   return (
     <div classsName="ui container comments">
-      <div classsName="ui container comments">
+      <div classsName="comment">
+        <a href="/" className="Avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata"></div>
+          <span className="date">Today at 6:00pm</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+
+      <div classsName="comment">
+        <a href="/" className="Avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata"></div>
+          <span className="date">Today at 6:00pm</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+      <div classsName="comment">
         <a href="/" className="Avatar">
           <img alt="avatar" src={faker.image.avatar()} />
         </a>
