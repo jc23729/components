e7748a81cbd2b752c3d08c2bb236bd3ade751aa9
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-20 17:59:46 -0400

    update package.json

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
   return (
     <div classsName="ui container comments">
       <div classsName="ui container comments">
         <a href="/" className="Avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
             Sam
           </a>
-                  <div className="metadata"></div>
-                  <span className="date">Today at 6:00pm</span>
-              </div>
-              <div className="text">Nice blog post!</div>
+          <div className="metadata"></div>
+          <span className="date">Today at 6:00pm</span>
+        </div>
+        <div className="text">Nice blog post!</div>
       </div>
     </div>
   );
